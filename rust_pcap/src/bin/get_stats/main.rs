c92489dec989d0f536dcf94aba6c931a98df078e
Владимир <artemyev.vova@gmail.com>
2022-06-07 17:51:44 +0300

    добавил pcapng

--- rust_pcap/src/bin/get_stats/main.rs
+++ rust_pcap/src/bin/get_stats/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use pcap_parser::*;
-use pcap_parser::traits::PcapReaderIterator;
+use pcap_parser::traits::{PcapNGPacketBlock, PcapReaderIterator};
 use std::fs::File;
 use std::time::Instant;
 use rust_pcap::*;
@@ -22,64 +22,44 @@ fn main() {
     let mut pkt_sizes = Vec::new();
     let mut last_time = None;
     let mut pkt_times = Vec::new();
-    let mut reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader");
-    loop {
-        match reader.next() {
-            Ok((offset, block)) => {
-                num_blocks += 1;
-                match block {
-                    PcapBlockOwned::LegacyHeader(hdr) => {
-                        println!("{hdr:?}")
-                    }
-                    PcapBlockOwned::Legacy(b) => {
-                        pkt_sizes.push(b.data.len());
-                        if let Some(time) = last_time {
-                            pkt_times.push(time - b.ts_sec);
-                        }
-                        last_time = Some(b.ts_sec);
-
-                        let frame = Frame::new(b.data);
-                        // let eth = frame.get_layer::<Ethernet>().unwrap();
-                        if let Some(ipv4) = frame.get_layer::<IPv4>() {
-                            ip_count += 1;
-                            addresses.insert(ipv4.src);
-                            addresses.insert(ipv4.dst);
-                        }
-                        if let Some(udp) = frame.get_layer::<UDP>() {
-                            udp_count += 1;
-                            data_bytes += udp.payload.len();
-                        }
-                        if let Some(tcp) = frame.get_layer::<TCP>() {
-                            tcp_count += 1;
-                            ports.insert(tcp.src);
-                            ports.insert(tcp.dst);
-                            data_bytes += tcp.data.len();
-                        }
-                        if let Some(_) = frame.get_layer::<ICMP>() {
-                            icmp_count += 1;
-                        }
-                        if let Some(_) = frame.get_layer::<ARP>() {
-                            arp_count += 1;
-                        }
-                        if let Some(_) = frame.get_layer::<HTTP>() {
-                            http_count += 1;
-                        }
-                        if let Some(_) = frame.get_layer::<DHCP>() {
-                            dhcp_count += 1;
-                        }
-                        // break;
-                    }
-                    PcapBlockOwned::NG(_) => unreachable!(),
-                }
-                reader.consume(offset);
-            }
-            Err(PcapError::Eof) => break,
-            Err(PcapError::Incomplete) => {
-                reader.refill().unwrap();
-            }
-            Err(e) => panic!("error while reading: {:?}", e),
+    let mut collect_stats = |frame: Frame| -> bool  {
+        num_blocks += 1;
+        pkt_sizes.push(frame.data.len());
+        if let Some(time) = last_time {
+            pkt_times.push(time - frame.ts_sec);
         }
-    }
+        last_time = Some(frame.ts_sec);
+        if let Some(ipv4) = frame.get_layer::<IPv4>() {
+            ip_count += 1;
+            addresses.insert(ipv4.src);
+            addresses.insert(ipv4.dst);
+        }
+        if let Some(udp) = frame.get_layer::<UDP>() {
+            udp_count += 1;
+            data_bytes += udp.payload.len();
+        }
+        if let Some(tcp) = frame.get_layer::<TCP>() {
+            tcp_count += 1;
+            ports.insert(tcp.src);
+            ports.insert(tcp.dst);
+            data_bytes += tcp.data.len();
+        }
+        if let Some(_) = frame.get_layer::<ICMP>() {
+            icmp_count += 1;
+        }
+        if let Some(_) = frame.get_layer::<ARP>() {
+            arp_count += 1;
+        }
+        if let Some(_) = frame.get_layer::<HTTP>() {
+            http_count += 1;
+        }
+        if let Some(_) = frame.get_layer::<DHCP>() {
+            dhcp_count += 1;
+        }
+        false
+    };
+    // iter_over_pcap(file, collect_stats);
+    iter_over_pcapng(file, collect_stats);
     dbg!(num_blocks);
     dbg!(ip_count);
     dbg!(tcp_count);
@@ -105,4 +85,93 @@ fn main() {
         .map(|&t| (avg_time - t as f32).abs())
         .sum::<f32>() / pkt_times.len() as f32;
     dbg!(avg_delta_time);
+}
+
+
+fn iter_over_pcap(file: File, mut fun: impl FnMut(Frame) -> bool) {
+    let mut reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader");
+    loop {
+        match reader.next() {
+            Ok((offset, block)) => {
+                match block {
+                    PcapBlockOwned::LegacyHeader(hdr) => {
+                        println!("{hdr:?}")
+                    }
+                    PcapBlockOwned::Legacy(b) => {
+                        let frame = Frame::from_legacy(&b);
+                        if fun(frame) {
+                            break;
+                        }
+                    }
+                    PcapBlockOwned::NG(_) => unreachable!(),
+                }
+                reader.consume(offset);
+            }
+            Err(PcapError::Eof) => break,
+            Err(PcapError::Incomplete) => {
+                reader.refill().unwrap();
+            }
+            Err(e) => panic!("error while reading: {:?}", e),
+        }
+    }
+}
+
+fn iter_over_pcapng(file: File, mut fun: impl FnMut(Frame) -> bool) {
+    let mut reader = PcapNGReader::new(65536, file).expect("PcapNGReader");
+    let mut if_linktypes = Vec::new();
+    loop {
+        match reader.next() {
+            Ok((offset, block)) => {
+                match block {
+                    PcapBlockOwned::NG(Block::SectionHeader(ref _shb)) => {
+                        // starting a new section, clear known interfaces
+                        if_linktypes = Vec::new();
+                    }
+                    PcapBlockOwned::NG(Block::InterfaceDescription(ref idb)) => {
+                        if_linktypes.push(idb.linktype);
+                    }
+                    PcapBlockOwned::NG(Block::EnhancedPacket(ref epb)) => {
+                        assert!((epb.if_id as usize) < if_linktypes.len());
+                        let linktype = if_linktypes[epb.if_id as usize];
+                        // let res = pcap_parser::data::get_packetdata(epb.data, linktype, epb.caplen as usize);
+
+                        let frame = Frame::new(
+                            epb.packet_data(),
+                            epb.ts_low, epb.ts_high,
+                            epb.caplen, epb.orig_len(),
+                        );
+                        if fun(frame) { break; }
+                    }
+                    PcapBlockOwned::NG(Block::SimplePacket(ref spb)) => {
+                        assert!(if_linktypes.len() > 0);
+                        let linktype = if_linktypes[0];
+                        let blen = (spb.block_len1 - 16) as usize;
+                        // let res = pcap_parser::data::get_packetdata(spb.data, linktype, blen);
+
+                        let frame = Frame::new(
+                            spb.packet_data(),
+                            0, 0,
+                            0, spb.orig_len(),
+                        );
+                        if fun(frame) { break; }
+
+                    }
+                    PcapBlockOwned::NG(_) => {
+                        // can be statistics (ISB), name resolution (NRB), etc.
+                        eprintln!("unsupported block");
+                    }
+                    PcapBlockOwned::Legacy(_)
+                    | PcapBlockOwned::LegacyHeader(_) => unreachable!(),
+                }
+                reader.consume(offset);
+            }
+            Err(PcapError::Eof) => break,
+            Err(PcapError::Incomplete) => {
+                eprintln!("Could not read complete data block.");
+                eprintln!("Hint: the reader buffer size may be too small, or the input file nay be truncated.");
+                break;
+            }
+            Err(e) => panic!("error while reading: {:?}", e),
+        }
+    }
 }
--- rust_pcap/src/frame.rs
+++ rust_pcap/src/frame.rs
@@ -1,16 +1,27 @@
+use pcap_parser::LegacyPcapBlock;
 use crate::*;
 use crate::ethernet::Ethernet;
 
 #[derive(Layer)]
 pub struct Frame {
+    pub ts_sec: u32,
+    pub ts_usec: u32,
+    pub caplen: u32,
+    pub origlen: u32,
+    pub data: Vec<u8>,
+
     layers: Layers,
 }
 
 impl Frame {
-    pub fn new(data: &[u8]) -> Frame {
+    pub fn new(data: &[u8], ts_sec: u32, ts_usec: u32, caplen: u32, origlen: u32) -> Frame {
         let mut layers = Layers::default();
         layers.insert(Ethernet::new(data));
-        Frame { layers }
+        Frame { ts_sec, ts_usec, caplen, origlen, data: data.to_vec(), layers }
+    }
+
+    pub fn from_legacy(block: &LegacyPcapBlock) -> Frame {
+        Self::new(block.data, block.ts_sec, block.ts_usec, block.caplen, block.origlen)
     }
 }
 
