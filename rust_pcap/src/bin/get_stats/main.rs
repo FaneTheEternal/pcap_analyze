9a69d356bcfee35717aa4b5cd8fecb17e451826a
Владимир <artemyev.vova@gmail.com>
2022-07-08 15:40:17 +0300

    фикс стороннего бинаря

--- rust_pcap/src/bin/get_stats/main.rs
+++ rust_pcap/src/bin/get_stats/main.rs
@@ -26,9 +26,9 @@ fn main() {
         num_blocks += 1;
         pkt_sizes.push(frame.data.len());
         if let Some(time) = last_time {
-            pkt_times.push(time - frame.ts_sec);
+            pkt_times.push(time - frame.ts);
         }
-        last_time = Some(frame.ts_sec);
+        last_time = Some(frame.ts);
         if let Some(ipv4) = frame.get_layer::<IPv4>() {
             ip_count += 1;
             addresses.insert(ipv4.src);
@@ -58,8 +58,8 @@ fn main() {
         }
         false
     };
-    // iter_over_pcap(file, collect_stats);
-    iter_over_pcapng(file, collect_stats);
+    iter_over_pcap(file, collect_stats);
+    // iter_over_pcapng(file, collect_stats);
     dbg!(num_blocks);
     dbg!(ip_count);
     dbg!(tcp_count);
@@ -79,7 +79,7 @@ fn main() {
         .map(|&s| (avg_size - s as f32).abs())
         .sum::<f32>() / num_blocks as f32;
     dbg!(avg_deltas_size);
-    let avg_time = pkt_times.iter().sum::<u32>() as f32 / pkt_times.len() as f32;
+    let avg_time = pkt_times.iter().sum::<f64>() as f32 / pkt_times.len() as f32;
     dbg!(avg_time);
     let avg_delta_time = pkt_times.iter()
         .map(|&t| (avg_time - t as f32).abs())
@@ -89,89 +89,19 @@ fn main() {
 
 
 fn iter_over_pcap(file: File, mut fun: impl FnMut(Frame) -> bool) {
-    let mut reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader");
-    loop {
-        match reader.next() {
-            Ok((offset, block)) => {
-                match block {
-                    PcapBlockOwned::LegacyHeader(hdr) => {
-                        println!("{hdr:?}")
-                    }
-                    PcapBlockOwned::Legacy(b) => {
-                        let frame = Frame::from_legacy(&b);
-                        if fun(frame) {
-                            break;
-                        }
-                    }
-                    PcapBlockOwned::NG(_) => unreachable!(),
-                }
-                reader.consume(offset);
-            }
-            Err(PcapError::Eof) => break,
-            Err(PcapError::Incomplete) => {
-                reader.refill().unwrap();
-            }
-            Err(e) => panic!("error while reading: {:?}", e),
+    let pcap = Pcap::new(file);
+    for frame in pcap {
+        if fun(frame) {
+            break;
         }
     }
 }
 
 fn iter_over_pcapng(file: File, mut fun: impl FnMut(Frame) -> bool) {
-    let mut reader = PcapNGReader::new(65536, file).expect("PcapNGReader");
-    let mut if_linktypes = Vec::new();
-    loop {
-        match reader.next() {
-            Ok((offset, block)) => {
-                match block {
-                    PcapBlockOwned::NG(Block::SectionHeader(ref _shb)) => {
-                        // starting a new section, clear known interfaces
-                        if_linktypes = Vec::new();
-                    }
-                    PcapBlockOwned::NG(Block::InterfaceDescription(ref idb)) => {
-                        if_linktypes.push(idb.linktype);
-                    }
-                    PcapBlockOwned::NG(Block::EnhancedPacket(ref epb)) => {
-                        assert!((epb.if_id as usize) < if_linktypes.len());
-                        let linktype = if_linktypes[epb.if_id as usize];
-                        // let res = pcap_parser::data::get_packetdata(epb.data, linktype, epb.caplen as usize);
-
-                        let frame = Frame::new(
-                            epb.packet_data(),
-                            epb.ts_low, epb.ts_high,
-                            epb.caplen, epb.orig_len(),
-                        );
-                        if fun(frame) { break; }
-                    }
-                    PcapBlockOwned::NG(Block::SimplePacket(ref spb)) => {
-                        assert!(if_linktypes.len() > 0);
-                        let linktype = if_linktypes[0];
-                        let blen = (spb.block_len1 - 16) as usize;
-                        // let res = pcap_parser::data::get_packetdata(spb.data, linktype, blen);
-
-                        let frame = Frame::new(
-                            spb.packet_data(),
-                            0, 0,
-                            0, spb.orig_len(),
-                        );
-                        if fun(frame) { break; }
-
-                    }
-                    PcapBlockOwned::NG(_) => {
-                        // can be statistics (ISB), name resolution (NRB), etc.
-                        eprintln!("unsupported block");
-                    }
-                    PcapBlockOwned::Legacy(_)
-                    | PcapBlockOwned::LegacyHeader(_) => unreachable!(),
-                }
-                reader.consume(offset);
-            }
-            Err(PcapError::Eof) => break,
-            Err(PcapError::Incomplete) => {
-                eprintln!("Could not read complete data block.");
-                eprintln!("Hint: the reader buffer size may be too small, or the input file nay be truncated.");
-                break;
-            }
-            Err(e) => panic!("error while reading: {:?}", e),
+    let pcapng = PcapNG::new(file);
+    for frame in pcapng {
+        if fun(frame) {
+            break;
         }
     }
 }
